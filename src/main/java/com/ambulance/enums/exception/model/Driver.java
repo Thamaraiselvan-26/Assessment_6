973d6cfb136e27996c5a5da949649e693b0fb9bc
Thamaraiselvan-26 <thamaraiselvan2627@gmail.com>
2026-09-10 14:45:56 +0530

    Create Driver.java

---
+++ src/main/java/com/ambulance/enums/exception/model/Driver.java
@@ -0,0 +1,16 @@
+package com.ambulance.model;
+
+public class Driver {
+    private String driverId;
+    private String name;
+    private String contactNumber;
+
+    public Driver(String driverId, String name, String contactNumber) {
+        this.driverId = driverId;
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getDriverId() { return driverId; }
+    public String getName() { return name; }
+}
